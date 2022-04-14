e93a99ecf430ff3206f17b65f9eca38d068c783b
mimamch <mimamch28@gmail.com>
2022-04-14 07:37:00 +0700

    change

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,82 +4,85 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <>
+      <main className="pt-10 text-white/90">
+        <div className="container mx-auto py-10">
+          {' '}
+          <div className="flex justify-around ">
+            <div className="text-2xl font-semibold tracking-wider">
+              MImamCh.
+            </div>
+            <div className="">
+              <a className="mx-5 opacity-80 hover:opacity-90" href="#">
+                Services
+              </a>
+              <a className="mx-5 opacity-80 hover:opacity-90" href="#">
+                How We Work
+              </a>
+              <a className="mx-5 opacity-80 hover:opacity-90" href="#">
+                Projects
+              </a>
+              <a className="mx-5 opacity-80 hover:opacity-90" href="#">
+                About
+              </a>
+            </div>
+            <div className="">
+              <a
+                className="rounded-full bg-slate-600 px-5 py-3 font-medium transition duration-200 ease-in-out hover:bg-slate-500"
+                href=""
+              >
+                Contact Me
+              </a>
+            </div>
+          </div>
         </div>
+        <section>
+          <div className="container mx-auto mt-10 w-6/12">
+            <div className="flex flex-col flex-wrap items-center justify-center text-center">
+              <h3 className="text-xl font-light">
+                Halo Semuanya 👋 Saya adalah
+              </h3>
+              <p className="bg-gradient-to-tr from-orange-500 to-indigo-500  bg-clip-text text-5xl font-semibold uppercase text-transparent">
+                Muhammad Imam Choirudin
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="mt-20">
+          <div className="container mx-auto flex w-10/12 items-center justify-center space-x-20">
+            <div className="w-6/12 px-3">
+              <h3 className="mb-5 text-center text-3xl font-medium">
+                About Me
+              </h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quidem, eaque at nemo, officia impedit perferendis unde aliquam
+                commodi beatae tempore similique maiores voluptate, tempora
+                optio et aut. Modi, numquam laborum, velit sequi commodi
+                reiciendis voluptas ipsa minima, sed esse eos. Lorem ipsum,
+                dolor sit amet consectetur adipisicing elit. Fuga quisquam
+                itaque ullam assumenda, numquam perspiciatis earum dolore
+                deserunt! Voluptatibus sapiente totam dolorum, reprehenderit
+                quas ut delectus dolore suscipit nemo placeat.
+              </p>
+              <a
+                className="mt-8 flex h-10 w-56 items-center justify-center rounded-full bg-slate-600 font-semibold transition duration-150 hover:bg-slate-500"
+                href="#"
+              >
+                See My Portofolio
+              </a>
+            </div>
+            <div className="w-4/12 px-3">
+              <img
+                className="h-80 w-80 rounded-full  object-cover"
+                src="https://placeimg.com/1000/1000/people"
+                alt="mimamch"
+              />
+            </div>
+          </div>
+        </section>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+    </>
   )
 }
 
